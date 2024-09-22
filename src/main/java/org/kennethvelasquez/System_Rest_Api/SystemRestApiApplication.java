2f51d46bf9c427c216e9d1e984931d56d70b3d8f
Kenneth Velasquez <kvelasquez@kinal.edu.gt>
2024-09-22 17:42:30 -0600

    Se creo el proyecto System_Rest_Api

---
+++ src/main/java/org/kennethvelasquez/System_Rest_Api/SystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.kennethvelasquez.System_Rest_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kennethvelasquez/System_Rest_Api/SystemRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kennethvelasquez.System_Rest_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
