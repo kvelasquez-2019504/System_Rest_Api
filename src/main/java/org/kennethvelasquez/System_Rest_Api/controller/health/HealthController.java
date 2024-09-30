7872fe15e7477f8ef52119f0b41e5a0c385ec114
kvelasquez-2019504 <kvelasquez-2019504@kinal.edu.gt>
2024-09-29 19:10:29 -0600

    Ejecuta la aplicacion correctamente

--- src/main/java/org/kennethvelasquez/System_Rest_Api/controller/health/HealthController.java
+++ src/main/java/org/kennethvelasquez/System_Rest_Api/controller/health/HealthController.java
@@ -1,5 +1,15 @@
 package org.kennethvelasquez.System_Rest_Api.controller.health;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class HealthController {
-    
+
+    @GetMapping("/health")
+    public String checkApi() {
+        return "<h1>The API is working ok!</h1>";
+    }
 }
